cf95b3237d69abb669f9a4117a9be786d64c90d6
MutaibaMunawar <mutaibachohan8@gmail.com>
2024-11-10 12:43:23 +0500

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Image from "next/image"; // Make sure to import Image from 'next/image'
+import Image from "next/image"; 
 import { Libre_Bodoni } from "next/font/google";
 
 const libreBodoni = Libre_Bodoni({
@@ -16,7 +16,9 @@ const Home = () => {
             IMPECCABLE CRAFTSMANSHIP AND FINESSE
           </h1>
           <p className={`${libreBodoni.className} text-[30px] leading-8 text-[#787054] ml-[171px]`}>
-            An example of intricate workmanship and detail, elegant necklaces and long and short chains form a part of our desirable collection.
+            An example of intricate workmanship and detail &apos;
+            elegant necklaces and long and short chains form a part of our
+             desirable collection.
           </p>
           <button className={`${libreBodoni.className} bg-[#A29875] text-[#FFFFFF] text-[30px] mt-8 rounded px-8 py-2 ml-[171px]`}>
             Explore Now
@@ -24,7 +26,7 @@ const Home = () => {
         </div>
         <div className="flex-1 flex flex-col items-center">
           <Image
-            src="/images/figma.svg"
+            src="/images/figma_img.svg"
             alt="jwellary_image"
             width={350}
             height={458}
